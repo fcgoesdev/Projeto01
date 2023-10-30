224347dc29b2ad9e105f6d063553634cb01a4de9
fcgoesdev <fcgoesdev@gmail.com>
2023-10-30 06:49:34 -0300

    Primeiro commit da nova brach

--- Hello_world.py
+++ Hello_world.py
@@ -5,4 +5,9 @@ frase = "Eu amo programar em Python !!!"
 for i in frase:
     print(i)
 
-    
+bombons = 10
+
+while bombons > 0:
+    print(f"Eu tenho {bombons} Bombons.")
+    bombons = bombons - 1
+    print(f"Comi 1 Bombom e fiquei com {bombons}!!!")
--- Readme.md
+++ Readme.md
@@ -3,5 +3,6 @@
 Este é o meu primeiro projeto utilizando o **Git** e o **Github**.
 
 - Adcionando mais uma modificação no Readme.md
+- Adicionada uma nova branch no projeto
 
 ## Mais uma modificação para fazer teste.
